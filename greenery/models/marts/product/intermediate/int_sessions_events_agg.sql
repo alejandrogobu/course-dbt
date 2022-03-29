fe281a90e63e0bde437510b58e41d4ce2e4f630c
Alejandro González Bueno <alejandro.gonzalez.bu@gmail.com>
2022-03-29 10:47:55 +0000

    add macro

--- greenery/models/marts/product/intermediate/int_sessions_events_agg.sql
+++ greenery/models/marts/product/intermediate/int_sessions_events_agg.sql
@@ -4,21 +4,12 @@ WITH fact_events AS (
     FROM {{ ref('fact_events') }}
     ),
 
-{% set sql_statement %}
-    SELECT DISTINCT quote_literal(event_type) AS event_type, event_type AS column_name FROM {{ ref('fact_events') }}
-{% endset %}
-
-{%- set event_types = dbt_utils.get_query_results_as_dict(sql_statement) -%}
-
-
 int_session_events_agg AS (
     SELECT 
         session_id
         ,created_at_utc
         ,user_id
-        {% for event_type in event_types['event_type'] %}
-        ,SUM(CASE WHEN event_type = {{event_type}} THEN 1 ELSE 0 END) AS {{ event_types['column_name'][loop.index0] }}
-        {% endfor %}
+        ,{{get_column_values(ref('fact_events'),'event_type')}}
     FROM fact_events
 
     {{ dbt_utils.group_by(3) }}
